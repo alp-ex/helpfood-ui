8e7c0699a01e21119c985deecf51f411a993cd14
alp-ex <mail.alp.ex@gmail.com>
2019-06-30 20:30:36 +0200

    try to use material ui by wrapping  bottom navigation component into nav component of the app

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,13 +1,78 @@
+import {
+    MdLocalGroceryStore,
+    MdRestaurantMenu,
+    MdViewAgenda,
+} from 'react-icons/md';
+import React, { ReactElement } from 'react';
+
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import { Link } from 'react-router-dom';
-import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+type Links = ReadonlyArray<{
+    path: string;
+    label: string;
+    icon: ReactElement;
+}>;
+
+const links: Links = [
+    {
+        path: '/',
+        label: 'Week',
+        icon: <MdViewAgenda />,
+    },
+    {
+        path: '/recipes',
+        label: 'Recipes',
+        icon: <MdRestaurantMenu />,
+    },
+    {
+        path: '/shoplist',
+        label: 'Shop list',
+        icon: <MdLocalGroceryStore />,
+    },
+];
+
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        position: 'fixed',
+        bottom: 0,
+        width: '100%',
+        background: '#ef698b',
+    },
+    icon: {
+        fontSize: '1em',
+    },
+});
 
 const Nav = () => {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
     return (
-        <ul>
-            <Link to="/">Planning</Link>
-            <Link to="/recipes">Recipes</Link>
-            <Link to="/shoplist">ShopList</Link>
-        </ul>
+        <BottomNavigation
+            value={value}
+            onChange={(event, newValue) => {
+                setValue(newValue);
+            }}
+            className={classes.root}
+        >
+            {links.map(link => (
+                <BottomNavigationAction
+                    showLabel={false}
+                    component={Link}
+                    key={link.path}
+                    to={link.path}
+                    className={classes.icon}
+                    // label={link.label}
+                    icon={link.icon}
+                />
+            ))}
+        </BottomNavigation>
     );
 };
 
