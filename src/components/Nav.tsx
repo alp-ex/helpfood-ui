e63e717f9882367a5bf17d0038a99e84c7ce58ec
alp_ex <alp_ex@alp.local>
2019-08-24 00:13:29 +0200

    add meal card to planning page, update nav and footer design regarding figma design

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,118 +1,103 @@
-import {
-    MdLocalGroceryStore,
-    MdRestaurantMenu,
-    MdViewAgenda,
-} from 'react-icons/md'
-import React, { ReactElement, HTMLProps } from 'react'
+// import {
+//   MdLocalGroceryStore,
+//   MdRestaurantMenu,
+//   MdViewAgenda
+// } from "react-icons/md";
+import React, { ReactElement, HTMLProps } from "react";
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 type Links = ReadonlyArray<{
-    path: string
-    label: string
-    icon: ReactElement
-}>
+  path: string;
+  label: string;
+  icon?: ReactElement;
+}>;
 
+// should I include a sorting logic here ? #overkill
 const links: Links = [
-    {
-        path: '/',
-        label: 'Week',
-        icon: <MdViewAgenda />,
-    },
-    {
-        path: '/recipes',
-        label: 'Recipes',
-        icon: <MdRestaurantMenu />,
-    },
-    {
-        path: '/shoplist',
-        label: 'Shop list',
-        icon: <MdLocalGroceryStore />,
-    },
-]
+  {
+    path: "/recipes",
+    label: "Recipes"
+    // icon: <MdRestaurantMenu />
+  },
+  {
+    path: "/",
+    label: "Planning"
+    // icon: <MdViewAgenda />
+  },
+  {
+    path: "/shoplist",
+    label: "Shop list"
+    // icon: <MdLocalGroceryStore />
+  }
+];
 
-interface BottomNavProps extends HTMLProps<HTMLElement> {
-    height?: string
-    backgroundColor?: string
+interface LinkTab extends HTMLProps<HTMLSpanElement> {
+  nbrOfSiblings: number;
 }
 
-const BottomNav = ({ children, height, backgroundColor }: BottomNavProps) => (
+const LinkTab = ({ children, nbrOfSiblings }) => (
+  <span
+    style={{
+      //   maxHeight: "100%",
+      cursor: "pointer",
+      width: `calc(100%/${nbrOfSiblings})`,
+      padding: "3%"
+    }}
+  >
+    {children}
+  </span>
+);
+
+export interface Props {}
+
+const Nav = () => {
+  const [value, setValue] = React.useState();
+
+  return (
     <nav
-        style={{
-            display: 'flex',
-            justifyContent: 'space-around',
-            alignItems: 'center',
-            position: 'fixed',
-            bottom: 0,
-            width: '100vw',
-            background: backgroundColor || '#ef698b',
-            height: height,
-        }}
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        width: "100%",
+        fontSize: "1em",
+        color: "#f0754e",
+        position: "fixed",
+        background: "#ffffff",
+        bottom: 0
+      }}
     >
-        {children}
+      {links.map(link => (
+        <LinkTab nbrOfSiblings={links.length} key={link.path}>
+          <Link
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              //   padding: "14px",
+              //   height: "100%",
+              ...(link === value && { filter: "brightness(42%)" })
+            }}
+            to={link.path}
+            // onMouseOver={evt =>
+            //   link !== value &&
+            //   (evt.currentTarget.style.background =
+            //     backgroundColorSelected || "#c7496929")
+            // }
+            // onMouseOut={evt =>
+            //   link !== value &&
+            //   (evt.currentTarget.style.background =
+            //     backgroundColor || "#ef698b")
+            // }
+            onClick={() => setValue(link)}
+          >
+            {link.label}
+          </Link>
+        </LinkTab>
+      ))}
     </nav>
-)
+  );
+};
 
-interface LinkIcon extends HTMLProps<HTMLSpanElement> {
-    nbrOfSiblings: number
-}
-
-const LinkIcon = ({ children, nbrOfSiblings }) => (
-    <span
-        style={{
-            maxHeight: '100%',
-            fontSize: '1em',
-            cursor: 'pointer',
-            width: `calc(100%/${nbrOfSiblings})`,
-        }}
-    >
-        {children}
-    </span>
-)
-
-export interface Props {
-    backgroundColor?: string
-    backgroundColorSelected?: string
-    height: string
-}
-
-const Nav = ({ backgroundColorSelected, backgroundColor, height }: Props) => {
-    const [value, setValue] = React.useState()
-
-    return (
-        <BottomNav height={height}>
-            {links.map(link => (
-                <LinkIcon nbrOfSiblings={links.length} key={link.path}>
-                    <Link
-                        style={{
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            padding: '14px',
-                            background:
-                                link === value
-                                    ? backgroundColorSelected || '#c7496991'
-                                    : backgroundColor || '#ef698b',
-                        }}
-                        to={link.path}
-                        onMouseOver={evt =>
-                            link !== value &&
-                            (evt.currentTarget.style.background =
-                                backgroundColorSelected || '#c7496929')
-                        }
-                        onMouseOut={evt =>
-                            link !== value &&
-                            (evt.currentTarget.style.background =
-                                backgroundColor || '#ef698b')
-                        }
-                        onClick={() => setValue(link)}
-                    >
-                        {link.icon}
-                    </Link>
-                </LinkIcon>
-            ))}
-        </BottomNav>
-    )
-}
-
-export default Nav
+export default Nav;
---
+++ src/fixtures/index.tsx
@@ -0,0 +1,42 @@
+const dishes = [
+  {
+    name: "cacoune",
+    description:
+      "It's like a real dish, but it's actually poo. That is my best dish when I receive people at my house.",
+    characteristics: {
+      isVegan: false,
+      isVegetarian: true,
+      warnings: ["not available for pregnant women"]
+    }
+  },
+  {
+    name: "pipoune",
+    description:
+      "It's like a real dish, but it's actually pee. That is my best dish when I receive people at my house.",
+    characteristics: {
+      isVegan: true,
+      isVegetarian: false,
+      warnings: ["not available for pregnant women"]
+    }
+  },
+  {
+    name: "tarte au cacoune",
+    description:
+      "It's like a real pie, but it's actually a poo pie. That is my best dish when I receive people at my house.",
+    characteristics: {
+      isVegan: false,
+      isVegetarian: false
+    }
+  },
+  {
+    name: "smoothie au pipoune",
+    description:
+      "It's like a real smoothie, but it's actually a pee smoothie. That is my best dish when I receive people at my house.",
+    characteristics: {
+      isVegan: true,
+      isVegetarian: true
+    }
+  }
+];
+
+export { dishes };
