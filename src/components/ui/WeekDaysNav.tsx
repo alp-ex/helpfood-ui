17a105797ac170183552f0c160a52c02a7a9c74c
alp-ex <mail.alp.ex@gmail.com>
2020-05-22 09:40:36 +0200

    remove missing dep

--- src/components/ui/WeekDaysNav.tsx
+++ src/components/ui/WeekDaysNav.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 import { SVGIcon, ArrowRightIcon } from '@components/icons'
 
 export interface Props {
